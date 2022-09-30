60e17720dd91eb05aff71fd071de52255ac094df
jordanstafforddev <jordanstafforddev@gmail.com>
2022-09-30 17:13:37 -0500

    update

--- Develop/index.js
+++ Develop/index.js
@@ -1,193 +1,83 @@
 // TODO: Include packages needed for this application
-
-const inquirer = require("inquirer");
-const fs = require("fs");
+var inquirer = require("inquirer");
+var generateMarkdown = require("./utils/generateMarkdown");
+var fs = require("fs");
+var util = require("util");
+var writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
-const questions = inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "Project Title?",
-      name: "projectTitle",
-      validate: (value) => {
-        if (value) {
-          return true;
-        } else {
-          return "Please insert a value";
-        }
-      },
-    },
-    {
-      type: "input",
-      message: "Project Description?",
-      name: "projectDescription",
-      validate: (value) => {
-        if (value) {
-          return true;
-        } else {
-          return "Please insert a value";
-        }
-      },
-    },
-    {
-      type: "input",
-      message: "Please enter a table of contents",
-      name: "projectTable",
-      validate: (value) => {
-        if (value) {
-          return true;
-        } else {
-          return "Please insert a value";
-        }
-      },
-    },
-    {
-      type: "input",
-      message: "How do you install the project?",
-      name: "projectInstall",
-      validate: (value) => {
-        if (value) {
-          return true;
-        } else {
-          return "Please insert a value";
-        }
-      },
-    },
-    {
-      type: "input",
-      message: "How do you use the project?",
-      name: "projectUse",
-      validate: (value) => {
-        if (value) {
-          return true;
-        } else {
-          return "Please insert a value";
-        }
-      },
-    },
-    {
-      type: "list",
-      message: "Which license did you use?",
-      name: "projectLicense",
-      choices: [
-        "No License",
-        "Apache License 2.0",
-        "GNU General Public License v3.0",
-        "MIT License",
-        "BSD 2-Clause 'Simplified' License",
-        "BSD 3-Clause 'New' or 'Revised' License",
-        "Boost Software License 1.0",
-        "Creative Commons Zero v1.0 Universal",
-        "Eclipse Public License 2.0",
-        "GNU Affero General Public License v3.0",
-        "GNU General Public License v2.0",
-        "GNU Lesser General Public License v2.1",
-        "Mozilla Public License 2.0",
-        "The Unlicense",
-      ],
-      validate: (value) => {
-        if (value) {
-          return true;
-        } else {
-          return "Please insert a value";
-        }
-      },
-    },
-    {
-      type: "input",
-      message:
-        "Who are the collaborators? Please include links to their GitHub profiles.",
-      name: "projectCredits",
-      validate: (value) => {
-        if (value) {
-          return true;
-        } else {
-          return "Please insert a value";
-        }
-      },
-    },
-    {
-      type: "input",
-      message: "Tests?",
-      name: "projectTests",
-      validate: (value) => {
-        if (value) {
-          return true;
-        } else {
-          return "Please insert a value";
-        }
-      },
-    },
-    {
-      type: "input",
-      message: "Questions?",
-      name: "projectQuestions",
-      validate: (value) => {
-        if (value) {
-          return true;
-        } else {
-          return "Please insert a value";
-        }
-      },
-    },
-  ])
-  .then(
-    ({
-      projectTitle,
-      projectDescription,
-      projectTable,
-      projectInstall,
-      projectUse,
-      projectLicense,
-      projectCredits,
-      projectTests,
-      projectQuestions,
-    }) => {
-      const template = `# ${projectTitle}
-    
-    Description:
-    ${projectDescription}
-    Table:
-    ${projectTable}
-    How to Install:
-    ${projectInstall}
-    How to Use:
-    ${projectUse}
-    License:
-    ${projectLicense}
-    Contributors:
-    ${projectCredits}
-    Tests:
-    ${projectTests}
-    Questions:
-    ${projectQuestions}`;
+var questions = [
+  {
+    type: "input",
+    message: "Please enter your github username",
+    name: "projectUsername",
+  },
+  {
+    type: "input",
+    message: "Please enter your project's title",
+    name: "projectTitle",
+  },
+  {
+    type: "input",
+    message:
+      "Please enter description of your project (motivations, purpose, etc.)",
+    name: "projectDescription",
+  },
+  {
+    type: "input",
+    message:
+      "Please enter the steps required to install your project (step by step)",
+    name: "projectInstallation",
+  },
+  {
+    type: "input",
+    message: "Please enter the instructions and examples for use.",
+    name: "projectUsage",
+  },
+  {
+    type: "input",
+    message: "Please provide the github account names of all collaborators",
+    name: "projectCollaborators",
+  },
+  {
+    type: "list",
+    message: "Please enter the license your project possesses",
+    name: "projectLicense",
+    choices: [
+      "None",
+      "Apache License 2.0",
+      "GNU General Public License v3.0",
+      "MIT License",
+      'BSD 2-Clause "Simplified" License',
+      'BSD 3-Clause "New" or "Revised" License',
+      "Boost Software License 1.0",
+      "Creative Commons Zero v1.0 Universal",
+      "Eclipse Public License 2.0",
+      "GNU Affero General Public License v3.0",
+      "GNU General Public License v2.0",
+      "GNU Lesser General Public License v2.1",
+      "Mozilla Public License 2.0",
+      "The Unlicense",
+    ],
+  },
+];
 
-      // # Contact
-      // * GitHub :${git}
-      // * Linkedin :${linkedin}
-      // * E-mail :${email};
-
-      writeToFile(projectTitle, template);
-    }
-  );
+var promptQuestions = () => {
+  return inquirer.prompt(questions);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-  fs.writeFile(
-    `./${fileName.toLowerCase().split("  ").join("")}.md`,
-    data,
-    (err) => {
-      if (err) {
-        console.log(err);
-      }
-      console.log("The README has been created!");
-    }
-  );
+  return writeFileAsync(fileName, data);
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+  var input = await promptQuestions();
+  var content = generateMarkdown(input);
+  await writeToFile("./README.md", content);
+  console.log("README.md has been generated");
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,46 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  return `# ${data.projectTitle}
+  
+  ## Table of Contents
+  * [Project Creator](#projectUsername)
+  * [Project Description](#projectDescription)
+  * [Project Installation](#projectInstallation)
+  * [Project Usage](#projectUsage)
+  * [Project Collaborators](#projectCollaborators)
+  * [Project License](#projectLicense)
+  
+  ## Created by ${data.projectUsername}
+  (https://github.com/${data.projectUsername})
+  
+  ## Project Description
+  ${data.projectDescription}
+  
+  ## Project Installation
+  ${data.projectInstallation}
+  
+  ## Project Usage
+  ${data.projectUsage}
+  
+  ## Project Collaborators
+  ${data.projectCollaborators}
+  
+  ## License
+  Repository possesses the ${data.projectLicense} license.
+  
+    `;
 }
 
 module.exports = generateMarkdown;
